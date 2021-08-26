9fdea8643ccb0f59a9ab21eecf6bed30e626fa27
孙许 <2572012446@qq.com>
2021-08-27 00:14:01 +0800

    初始化

---
+++ src/main/java/com/sunxu/eureka/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.sunxu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 孙许
+ * @version 1.0
+ * @date 2021/8/26 23:20
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
